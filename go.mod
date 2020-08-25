0771fdbb52f2e2f7063ec1a241339db7b3dbf56c
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2020-08-26 00:32:16 +0300

    generator some fix and change

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/frkn2076/SudokuAPI
 
 go 1.14
 
-require github.com/gofiber/fiber v1.14.4
+require (
+	github.com/gofiber/fiber v1.14.4
+	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987 // indirect
+)
--- main.go
+++ main.go
@@ -1,66 +1,83 @@
 package main
 
 import (
-	"github.com/gofiber/fiber"
 	"math"
 	"math/rand"
 	"strconv"
+
+	"github.com/gofiber/fiber"
 )
 
 func main() {
 	app := fiber.New()
 
-	app.Post("/", func(c *fiber.Ctx) {
+	app.Post("/sudoku/generate", func(c *fiber.Ctx) {
 		level, err := strconv.Atoi(c.Body())
 
 		if err != nil {
-			c.Send(err)
-		} else{
+			c.Send("Send a number 1 to 10 as request\n1  -> Easiest\n10 -> Hardest")
+		} else if level < 1 || level > 10 {
+			c.Send("You should send a number 1 to 10.")
+		} else {
+
 			sequence := make([]int, 18)
-		for i := range sequence {
-			sequence[i] = i % 9 + 1
-		}
-		sample := make([][]int, 9)
-		for i := 0; i < 9; i++ {
-			temp := make([]int, 9)
-			copy(temp,sequence[9-int(math.Floor(float64(i/3)))-(i%3)*3 : 9-int(math.Floor(float64(i/3)))-(i%3)*3+9])
-			sample[i] = temp
-		}
+			for i := range sequence {
+				sequence[i] = i%9 + 1
+			}
 
-		for i := 0; i < 25; i++ {
+			sample := make([][]int, 9)
+			for i := 0; i < 9; i++ {
+				temp := make([]int, 9)
+				copy(temp, sequence[9-int(math.Floor(float64(i/3)))-(i%3)*3:9-int(math.Floor(float64(i/3)))-(i%3)*3+9])
+				sample[i] = temp
+			}
 
-			rand1 := rand.Intn(9) + 1
-			rand2 := rand.Intn(9) + 1
-			
-			if(rand1 != rand2){
-				for a := 0; a < 9; a++ {
-					for b := 0; b < 9; b++ {
-						if(sample[a][b] == rand1){
-							sample[a][b] = rand2
-						} else if(sample[a][b] == rand2){
-							sample[a][b] = rand1
-						}				
+			for i := 0; i < 25; i++ {
 
+				rand1 := rand.Intn(9) + 1
+				rand2 := rand.Intn(9) + 1
+
+				if rand1 != rand2 {
+					for a := 0; a < 9; a++ {
+						for b := 0; b < 9; b++ {
+							if sample[a][b] == rand1 {
+								sample[a][b] = rand2
+							} else if sample[a][b] == rand2 {
+								sample[a][b] = rand1
+							}
+
+						}
 					}
 				}
 			}
-		}
-		
-		for i := 0; i < level; i++ {
-			for i := 0; i < level * 2; i++ {
+
+			for i := 0; i < level*10; i++ {
 				sample[rand.Intn(9)][rand.Intn(9)] = 0
 			}
+
+			c.Send(sample)
 		}
 
-		}
+	})
 
-	
-		
+	app.Post("/sudoku/validate", func(c *fiber.Ctx) {
+		req := new([][]int)
+		if err := c.BodyParser(req); err != nil {
+			c.Send("Expected 9x9 integer array")
+		} else {
+			// expectedSorted := []int{1,2,3,4,5,6,7,8,9}
+
+			// //row check
+			// for i := 0; i < 9; i++ {
+			// 	temp := (*req)[i]
+			// 	if temp != expectedSorted {
+			// 		return false
+			// 	}
+			// }	
+
+
+		}
 	})
 
 	app.Listen(3000)
-}
-
-type SudokuLevel struct {
-    level int
 }

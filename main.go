06d878ef36ce957d9d171fd5324123ac6347ceeb
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2020-09-13 14:10:29 +0300

    vendor folder removed, docker file added

--- main.go
+++ main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"SudokuAPI/Router"
-	"os"
 )
 
 func main() {
 	router := Router.SetupRouter()
-	// router.Run(":3001")        //uncomment for local
-	router.Run(os.Getenv("PORT")) //comment for local
+	router.Run(":3000")        			//uncomment for local
+	// router.Run(os.Getenv("PORT")) 	//comment for local
 }

d3ef6a3a250cdfc4aa35b7e3a060510fce1ab412
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2020-09-13 20:05:33 +0300

    fix

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/frkn2076/SudokuAPI
 
 go 1.14
+replace app => github.com/frkn2076
 
 require github.com/gin-gonic/gin v1.6.3 // indirect
+
 
--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"app/SudokuAPI/Router"
+	"os"
 )
 
 func main() {
 	router := Router.SetupRouter()
 	// router.Run(":3000")        			//uncomment for local
 	router.Run(os.Getenv("PORT")) 	//comment for local
-	
+
 }

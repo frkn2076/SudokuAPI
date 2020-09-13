e47b449b65f34c0afe287ffa33fedeb82014aa18
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2020-09-13 13:15:57 +0300

    port changed as environment port

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"SudokuAPI/Router"
+	"os"
 )
 
 func main() {
 	router := Router.SetupRouter()
-	router.Run(":3001")
+	// router.Run(":3001")        //uncomment for local
+	router.Run(os.Getenv("PORT")) //comment for local
 }
-
-
